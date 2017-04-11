d3899b37c1f81a00ea1440f9aef42fad693266a2
phycholee <962109025@qq.com>
2017-04-11 23:46:59 +0800

    添加加密算法工具类

--- src/main/java/com/phycholee/blog/authorization/controller/LoginController.java
+++ src/main/java/com/phycholee/blog/authorization/controller/LoginController.java
@@ -1,9 +1,13 @@
 package com.phycholee.blog.authorization.controller;
 
+import com.phycholee.blog.service.AdminService;
+import com.phycholee.blog.utils.EncryptUtil;
 import com.phycholee.blog.utils.JsonData;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  * Created by PhychoLee on 2017/4/10 23:21.
@@ -13,8 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("login")
 public class LoginController {
 
+    @Autowired
+    private AdminService adminService;
+
     @RequestMapping(value = "admin",method = RequestMethod.POST)
     public JsonData adminLogin(String username, String password){
+        try {
+            String salt = EncryptUtil.generateSalt();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
 
         return null;
